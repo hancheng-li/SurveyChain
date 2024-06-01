1cd6f8b9e6eb3d2ed14f8cf39fa80a0e7fe13c0d
shashank790 <shashank790@ucsb.edu>
2024-05-31 18:18:31 -0700

    create+get survey

--- SurveyChain/src/SurveySystem.sol
+++ SurveyChain/src/SurveySystem.sol
@@ -12,7 +12,7 @@ contract SurveySystem {
     struct Survey {
         string description;
         uint256 id;
-        uint256[] choices;
+        string[] choices; // Descriptions of the choices (changed to string[])
         uint256 startTime;
         uint256 endTime;
         uint256 maxVotes;
@@ -20,10 +20,98 @@ contract SurveySystem {
         uint256 reward;
         address[] voters;
         bool isClosed;
+        // Added in line below, Address of the survey creator
+        address owner;
     }
 
-    function createSurvey(string memory _description, uint256[] memory _choices, uint256 duration, uint256 _maxVotes, uint256 _reward) public {
-        
+    // Function to create a new survey
+    function createSurvey(string memory _description, string[] memory _choices, uint256 duration, uint256 _maxVotes, uint256 _reward) public {
+        require(_choices.length > 0, "Survey must have at least one choice");
+        require(duration > 0, "Survey duration must be greater than zero");
+        require(_maxVotes > 0, "Max votes must be greater than zero");
+        require(_reward > 0, "Reward must be greater than zero");
+
+        uint256 surveyId = surveys.length;  // Use the length of the surveys array as the new survey ID
+        surveys.push();
+
+        Survey storage newSurvey = surveys[surveyId];
+        newSurvey.description = _description;
+        newSurvey.id = surveyId;
+        newSurvey.choices = _choices;
+        newSurvey.startTime = block.timestamp;
+        newSurvey.endTime = block.timestamp + duration;
+        newSurvey.maxVotes = _maxVotes;
+        newSurvey.votes = new uint256[](_choices.length);  // Initialize the votes array
+        newSurvey.reward = _reward;
+        newSurvey.isClosed = false;
+        newSurvey.owner = msg.sender;
+    }
+
+    // Getter functions to retrieve survey details in parts
+    function getSurveyBasicDetails(uint256 _surveyId) public view returns (
+        string memory description,
+        uint256 id,
+        string[] memory choices,
+        uint256 startTime,
+        uint256 endTime,
+        uint256 maxVotes,
+        uint256 reward,
+        bool isClosed,
+        address owner
+    ) {
+        Survey storage survey = surveys[_surveyId];
+        return (
+            survey.description,
+            survey.id,
+            survey.choices,
+            survey.startTime,
+            survey.endTime,
+            survey.maxVotes,
+            survey.reward,
+            survey.isClosed,
+            survey.owner
+        );
+    }
+
+    // Getter functions to retrieve survey details in parts
+    function getSurveyDescription(uint256 _surveyId) public view returns (string memory) {
+        return surveys[_surveyId].description;
+    }
+
+    function getSurveyChoices(uint256 _surveyId) public view returns (string[] memory) {
+        return surveys[_surveyId].choices;
+    }
+
+    function getSurveyStartTime(uint256 _surveyId) public view returns (uint256) {
+        return surveys[_surveyId].startTime;
+    }
+
+    function getSurveyEndTime(uint256 _surveyId) public view returns (uint256) {
+        return surveys[_surveyId].endTime;
+    }
+
+    function getSurveyMaxVotes(uint256 _surveyId) public view returns (uint256) {
+        return surveys[_surveyId].maxVotes;
+    }
+
+    function getSurveyReward(uint256 _surveyId) public view returns (uint256) {
+        return surveys[_surveyId].reward;
+    }
+
+    function getSurveyIsClosed(uint256 _surveyId) public view returns (bool) {
+        return surveys[_surveyId].isClosed;
+    }
+
+    function getSurveyOwner(uint256 _surveyId) public view returns (address) {
+        return surveys[_surveyId].owner;
+    }
+
+    function getSurveyVotes(uint256 _surveyId) public view returns (uint256[] memory) {
+        return surveys[_surveyId].votes;
+    }
+
+    function getSurveyVoters(uint256 _surveyId) public view returns (address[] memory) {
+        return surveys[_surveyId].voters;
     }
 
     function vote(uint256 _surveyId, uint256 _choice) public {
@@ -34,7 +122,4 @@ contract SurveySystem {
         
     }
 
-    function getSurvey(uint256 _surveyId) public view returns (Survey memory) {
-        
-    }
 }
--- SurveyChain/test/SurveySystem.t.sol
+++ SurveyChain/test/SurveySystem.t.sol
@@ -7,9 +7,47 @@ import {SurveySystem} from "../src/SurveySystem.sol";
 contract SurveySystemTest is Test {
     SurveySystem public surveySystem;
 
-    function setUp() public {}
+    function setUp() public {
+        surveySystem = new SurveySystem();
+    }
 
-    function testSurveyCreation() public {}
+    function testCreateSurvey() public {
+        // Set up survey parameters
+        string memory description = "Test Survey";
+        string[] memory choices = new string[](2);
+        choices[0] = "Option 1";
+        choices[1] = "Option 2";
+        uint256 duration = 1 weeks;
+        uint256 maxVotes = 100;
+        uint256 reward = 10 ether;
 
+        // Simulate a user creating a survey
+        address user = address(this);
+        vm.prank(user);
+        surveySystem.createSurvey(description, choices, duration, maxVotes, reward);
+
+        // Retrieve and verify survey details in parts to avoid stack too deep error
+        assertEq(surveySystem.getSurveyDescription(0), description);
+        assertEq(surveySystem.getSurveyChoices(0).length, 2);
+        assertEq(surveySystem.getSurveyChoices(0)[0], "Option 1");
+        assertEq(surveySystem.getSurveyChoices(0)[1], "Option 2");
+        assertEq(surveySystem.getSurveyStartTime(0), block.timestamp);
+        assertEq(surveySystem.getSurveyEndTime(0), block.timestamp + duration);
+        assertEq(surveySystem.getSurveyMaxVotes(0), maxVotes);
+        assertEq(surveySystem.getSurveyReward(0), reward);
+        assertEq(surveySystem.getSurveyIsClosed(0), false);
+        assertEq(surveySystem.getSurveyOwner(0), user);
+
+        // Verify that the votes array is initialized correctly
+        uint256[] memory votes = surveySystem.getSurveyVotes(0);
+        assertEq(votes.length, 2);
+        assertEq(votes[0], 0);
+        assertEq(votes[1], 0);
+
+        // Verify that the voters array is initialized correctly
+        address[] memory voters = surveySystem.getSurveyVoters(0);
+        assertEq(voters.length, 0);
+        
+    }
     function testSurveyVoting() public {}
 }
